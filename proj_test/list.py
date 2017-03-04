bb5e04222f7edd77bb461c9a59d4b1efd6eef8eb
Sergey Shimanskiy <flyer8@yandex.ru>
2017-03-04 20:38:39 +0300

    Added

--- proj_test/list.py
+++ proj_test/list.py
@@ -1,8 +1,11 @@
-list_name = ["Es","Ches","es","ches"]
+# List generator
+list_name = ["EC","ec","ECh","ech","Es","es","Ches","ches","Esc","esc","Esch","esch","Churikova","churikova","Echurikova","echurikova","Cantabile","cantabile"]
 list_sym = ["!", "@", "#", "$"]
-list_date = ["110581", "11051981"]
+list_date = ["1105", "110581", "11051981", "1981","81"]
 list = [n+s+d for n in list_name for s in list_sym for d in list_date]
+list2 = [n+d for n in list_name for d in list_date]
 #print('\n'.join(list))
 f = open("chur.txt","w")
-f.write('\n'.join(list));
+f.write('\n'.join(list))
+f.write('\n'.join(list2))
 f.close()
